262be599679b6fef76f85d07d06f93f40ca291ba
Jason J. Rotello <jason@jrotello.com>
2017-06-29 13:15:57 -0500

    Split out an Install-Profile script to reduce load time

---
+++ Install-Profile.ps1
@@ -0,0 +1,38 @@
+################################################################################################
+#### Ensure git is installed ###################################################################
+################################################################################################
+try {
+    # Check if git is on PATH, i.e. Git already installed on system
+    Get-command -Name "git" -ErrorAction Stop | Out-Null
+} catch {
+    Write-Warning "Missing git support, install git with 'choco install git.install'"
+}
+
+################################################################################################
+#### Install modules ###########################################################################
+################################################################################################
+function installModule {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]]$Names
+    )
+
+    foreach ($name in $Names) {
+        if ((Get-InstalledModule $name -ErrorAction Ignore) -eq $null) {
+            Write-Warning "Missing $name module, installing..."
+            Install-Module $name
+        }
+    }
+}
+
+installModule('posh-git', 'AzureRM', 'AzureAD', 'PsHosts')
+
+################################################################################################
+#### Clone git repositories ####################################################################
+################################################################################################
+if ((Test-Path $PSScriptRoot\ps-motd\ -PathType Container) -eq $false) { 
+    Write-Warning "Missing 'ps-motd' repository, cloning from GitHub" 
+    git clone https://github.com/mmillar-bolis/ps-motd.git $PSScriptRoot\ps-motd\ 
+} 
+
+Write-Host 'Profile installation complete!' -ForegroundColor Green
--- profile.ps1
+++ profile.ps1
@@ -1,23 +1,9 @@
-try {
-    # Check if git is on PATH, i.e. Git already installed on system
-    Get-command -Name "git" -ErrorAction Stop | Out-Null
-
-    # if (-not (Test-Path $PSScriptRoot\ps-motd\Get-MOTD.ps1)) {
-    #     Write-Warning "Missing 'ps-motd', cloning from GitHub"
-    #     git clone https://github.com/mmillar-bolis/ps-motd.git $PSScriptRoot\ps-motd\
-    # }
-} catch {
-    Write-Warning "Missing git support, install git with 'choco install git.install'"
-}
-
-try {
-    Import-Module -Name "posh-git" -ErrorAction Stop | Out-Null
-    $gitStatus = $true
-    $global:GitPromptSettings.EnableFileStatus = $false
-} catch {
-    Write-Warning "Missing git support, install posh-git with 'Install-Module posh-git'"
-    $gitStatus = $false
-}
+################################################################################################
+#### Set up prompt, adding the git prompt parts from posh-get ##################################
+################################################################################################
+Import-Module -Name "posh-git"
+$gitStatus = $true
+$global:GitPromptSettings.EnableFileStatus = $false
 
 function checkGit($Path) {
     if (Test-Path -Path (Join-Path $Path '.git/') ) {
@@ -30,7 +16,6 @@ function checkGit($Path) {
     }
 }
 
-# Set up a Cmder prompt, adding the git prompt parts inside git repos
 function global:prompt {
     $realLASTEXITCODE = $LASTEXITCODE
     $Host.UI.RawUI.ForegroundColor = "White"
@@ -43,11 +28,26 @@ function global:prompt {
     return " "
 }
 
+################################################################################################
+#### Login to Azure if necessary ###############################################################
+################################################################################################
+try {
+    $cred_path = '~\.pscredentials\azure.credential'
+    if (Test-Path $cred_path -PathType Leaf) {
+        Login-AzureRmAccount -Credential (Import-Clixml $cred_path)
+    }
+} finally {
+    Remove-Variable cred_path
+}
+
+################################################################################################
+#### Miscellanous ##############################################################################
+################################################################################################
+Set-Location ~
+
 if ((Get-Host).UI.RawUI -ne $null) {
     (Get-Host).UI.RawUI.WindowTitle = "[$env:COMPUTERNAME] $($(Get-Host).UI.RawUI.WindowTitle)"
 }
 
 # . "$PSScriptRoot\ps-motd\Get-MOTD.ps1"
 # Get-MOTD
-
-Set-Location ~
